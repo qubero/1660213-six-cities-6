7ca6b938bb24ef1ecd57f37b0d4b05bf2fcc9bff
Mamaev Vladislav <mamaev757@gmail.com>
2021-02-11 22:37:10 +0300

    Add routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import AuthScreen from '../auth-screen/auth-screen';
+import RoomScreen from '../room-screen/room-screen';
+import FavoritesScreen from '../favorites-screen/favorites-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 const App = (props) => {
   const {placesCount} = props;
 
   return (
-    <MainScreen placesCount={placesCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen placesCount={placesCount} />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen/>
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen/>
+        </Route>
+        <Route exact path="/offer/:id">
+          <RoomScreen/>
+        </Route>
+        <Route>
+          <NotFoundScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
